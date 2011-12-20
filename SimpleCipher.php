60e35b3c70009b11107cf0650d3f71037e45a148
pomaxa <pomaxa@gmail.com>
2011-12-21 00:29:49 +0200

    simple cipher

---
+++ SimpleCipher.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: pomaxa
+ * Date: 12/20/11
+ * Time: 11:36 PM
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ example.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: pomaxa
+ * Date: 12/20/11
+ * Time: 11:40 PM
+ * To change this template use File | Settings | File Templates.
+ */
