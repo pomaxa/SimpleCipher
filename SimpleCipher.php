6a49fad509fe16b70eb878157b70494991b44284
pomaxa <pomaxa@gmail.com>
2011-12-21 00:32:23 +0200

    SimpleCipher static realization

--- SimpleCipher.php
+++ SimpleCipher.php
@@ -1,8 +1,40 @@
 <?php
 /**
- * Created by JetBrains PhpStorm.
- * User: pomaxa
- * Date: 12/20/11
- * Time: 11:36 PM
- * To change this template use File | Settings | File Templates.
+ * Simple Encode/Decode class.
+ * @author: pomaxa
  */
+
+class SimpleCipher
+{
+    protected static $password = 'myCoolSecretPassWord';
+    protected static $type = 'aes128';
+
+    public static function setPassword($password = 'myCoolSecret')
+    {
+        self::$password = $password;
+    }
+
+    public static function strongRandom($num_bytes, $raw = false)
+    {
+        $rand = openssl_random_pseudo_bytes($num_bytes);
+        if (!$raw) {
+            $rand = base64_encode($rand);
+        }
+        return $rand;
+    }
+
+    public static function encrypt($data)
+    {
+        $iv = base64_decode(SimpleCipher::strongRandom(16) );
+        $encrypted = openssl_encrypt($data, self::$type, self::$password, false, $iv);
+        return $iv . $encrypted;
+    }
+
+    public static function decrypt($data)
+    {
+        $iv = substr($data, 0, 16);
+        $data = substr($data, 16);
+        $decrypted = openssl_decrypt($data, self::$type, self::$password, false, $iv);
+        return $decrypted;
+    }
+}
--- example.php
+++ example.php
@@ -6,3 +6,15 @@
  * Time: 11:40 PM
  * To change this template use File | Settings | File Templates.
  */
+
+require 'SimpleCipher.php';
+
+//Cipher::setPassword('test');
+
+
+$msg = 'Hello !';
+echo "Original value" . $msg."\n";
+$encrypted = SimpleCipher::encrypt($msg);
+echo "Encrypted value: " . $encrypted."\n";
+$decrypted = SimpleCipher::decrypt($encrypted);
+echo "Decrypted value: " . $decrypted."\n";
