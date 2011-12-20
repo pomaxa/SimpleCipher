2a4ec7abe6c674542204307d9d365997a63ea0e1
pomaxa <pomaxa@gmail.com>
2011-12-21 00:33:54 +0200

    example comments

--- example.php
+++ example.php
@@ -1,10 +1,7 @@
 <?php
 /**
- * Created by JetBrains PhpStorm.
- * User: pomaxa
- * Date: 12/20/11
- * Time: 11:40 PM
- * To change this template use File | Settings | File Templates.
+ * Example of usage.
+ * @author: pomaxa
  */
 
 require 'SimpleCipher.php';
